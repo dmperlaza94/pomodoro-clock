0f28d935c6642a5cd7df33c1365e9a8934602f6e
dmpeprlaza94 <marcelaperlaza-094@hotmail.com>
2020-06-06 00:47:52 -0500

    inserte commponenetes funcionales

---
+++ src/componentes/TimeLeft.js
@@ -0,0 +1,12 @@
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
+import React, { useState } from "react";
+
+momentDurationFormatSetup(moment);
+
+const TimeLeft = ({ sessionLength }) => {
+  const [timeLeft, setTime] = useState(sessionLength);
+  const formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss");
+  return <div>{formattedTimeLeft}</div>;
+};
+export default TimeLeft;
